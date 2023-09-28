77628ff3798cf2f4f229699d9d4252288cf407c2
Eyad Gad <57005526+eyadgad@users.noreply.github.com>
2023-09-27 21:28:15 -0400

    Update

--- udp_client.py
+++ udp_client.py
@@ -1,38 +1,20 @@
-# Assignment: Simple Chat Room - Client Code Implementation
+# Assignment: UDP Simple Chat Room - UDP Client Code Implementation
 
 # **Libraries and Imports**: 
 #    - Import the required libraries and modules. 
-#    You may need the below libraries for the client.
+#    You may need sys, socket, argparse, select, threading (or _thread) libraries for the client implementation.
 #    Feel free to use any libraries as well.
-import sys
-import argparse
-import select
-import socket
-import threading # u can use '_thread'
+
 
 # **Global Variables**:
 #    - IF NEEDED, Define any global variables that will be used throughout the code.
 
 # **Function Definitions**:
 #    - In this section, you will implement the functions you will use in the client side.
-#    - If you will use and implement the below functions, please don't edit the namings.
 #    - Feel free to add more other functions, and more variables.
 #    - Make sure that names of functions and variables are meaningful.
-#    - Take into consideration error handling, interrupts, client shutdown, and documentation.
-
-def receive_messages():
-    # Function to handle incoming messages from the server.
-    pass
-
-def send_message():
-    # Function to send a message to the server.
-    pass
-
-def display_message():
-    # Function to display a message from the server.
-    pass
-
-def run_client(clientSocket, clientname, serverAddr, serverPort):
+#    - Take into consideration error handling, interrupts,and client shutdown.
+def run(clientSocket, clientname, serverAddr, serverPort):
     # The main client function.
     # It should handle incoming server messages, send messages to the server,
     # manage the client's connection, and gracefully exit.
@@ -48,6 +30,6 @@ if __name__ == "__main__":
     clientname = args.name
     serverAddr = '127.0.0.1'
     serverPort = 9301
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 
-    run_client(clientSocket, clientname, serverAddr, serverPort)  # Calling the function to start the client.
+    run(clientSocket, clientname, serverAddr, serverPort)  # Calling the function to start the client.
