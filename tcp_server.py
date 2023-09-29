99f66dc4d1f9d2d4ef34b5c37cee608e7f2c5454
Eyad Gad <57005526+eyadgad@users.noreply.github.com>
2023-09-29 01:14:01 -0400

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -21,6 +21,14 @@ if __name__ == "__main__":
     server_port = 9301
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# Creating a TCP socket.
     server_socket.bind(('127.0.0.1', server_port))
-    clients = [] #list to add the connected client sockets , feel free to adjust it to other place
+    server_socket.listen(3) # size of the waiting_queue that stores the incoming requests to connect.
+    
+    # please note that listen() method is NOT for setting the maximum clients to connect to server.
+    # didnt get it? basically, when the process (assume process is man that execute the code line by line) is executing the accept() method on the server side, 
+    # the process holds or waits there until a client sends a request to connect and then the process continue executing the rest of the code.
+    # good! what if there is another client wants to connect and the process on the server side isnt executing the accept() method at the same time,
+    # Now listen() method joins the party to solve this issue, it lets the incoming requests from the other clients to be stored in a queue or list until the process execute the accept() method.
+    # the size of the queue is set using listen(size). IF YOU STILL DONT GET IT, SEND ME AN EMAIL.
 
+    clients = [] #list to add the connected client sockets , feel free to adjust it to other place
     run(server_socket,server_port)# Calling the function to start the server.
